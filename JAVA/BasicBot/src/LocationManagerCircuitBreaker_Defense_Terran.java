3b8aff91204e7f88fbcd6430d01a26c1819a8745
agenthsh <agenthsh@gmail.com>
2018-07-24 19:59:57 +0900

    Defense Terran 관련 java 파일 추가

---
+++ JAVA/BasicBot/src/LocationManagerCircuitBreaker_Defense_Terran.java
@@ -0,0 +1,384 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import bwapi.TilePosition;
+import bwta.BWTA;
+import bwta.BaseLocation;
+import bwta.Chokepoint;
+
+// 서킷 브레이커 맵
+public class LocationManagerCircuitBreaker_Defense_Terran extends LocationManager {
+    private static final int ONE_CLOCK = 0;
+    private static final int FIVE_CLOCK = 1;
+    private static final int SEVEN_CLOCK = 2;
+    private static final int ELEVEN_CLOCK = 3;
+
+    @Override
+    public List<TilePosition> initBaseLocations() {
+	List<TilePosition> result = new ArrayList<>(4);
+
+	result.add(new TilePosition(117, 9)); // 1시
+	result.add(new TilePosition(117, 118)); // 5시
+	result.add(new TilePosition(7, 118)); // 7시
+	result.add(new TilePosition(7, 9)); // 11시
+
+	return result;
+    }
+
+    // 정찰할 위치(순서)를 설정한다.
+    @Override
+    public List<TilePosition> initSearchSequence() {
+	List<TilePosition> result = new ArrayList<>();
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시: 5시, 11시, 7시 순서로 정찰
+	    result.add(getBaseLocations(FIVE_CLOCK));
+	    result.add(getBaseLocations(ELEVEN_CLOCK));
+	    result.add(getBaseLocations(SEVEN_CLOCK));
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시: 7시, 1시, 11시 순서로 정찰
+	    result.add(getBaseLocations(SEVEN_CLOCK));
+	    result.add(getBaseLocations(ONE_CLOCK));
+	    result.add(getBaseLocations(ELEVEN_CLOCK));
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시: 11시, 5시, 1시 순서로 정찰
+	    result.add(getBaseLocations(ELEVEN_CLOCK));
+	    result.add(getBaseLocations(FIVE_CLOCK));
+	    result.add(getBaseLocations(ONE_CLOCK));
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시: 1시, 7시, 5시 순서로 정찰
+	    result.add(getBaseLocations(ONE_CLOCK));
+	    result.add(getBaseLocations(SEVEN_CLOCK));
+	    result.add(getBaseLocations(FIVE_CLOCK));
+	}
+
+	return result;
+    }
+
+    // 배럭, 팩토리, 스타포트와 같은 병력 훈련용 타일의 위치를 지정한다. Add on 건물 위치까지 고려해야 한다.
+    @Override
+    public List<TilePosition> initTrainingBuildings() {
+	List<TilePosition> result = new ArrayList<>();
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	    result.add(new TilePosition(118, 23));
+	    result.add(new TilePosition(118, 19));
+	    result.add(new TilePosition(118, 15));
+	    result.add(new TilePosition(111, 19));
+	    result.add(new TilePosition(111, 15));
+	    result.add(new TilePosition(111, 11));
+	    result.add(new TilePosition(111, 7));
+	    result.add(new TilePosition(105, 7));
+	    result.add(new TilePosition(105, 11));
+	    result.add(new TilePosition(105, 15));
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	    result.add(new TilePosition(118, 102));
+	    result.add(new TilePosition(118, 106));
+	    result.add(new TilePosition(118, 110));
+	    result.add(new TilePosition(111, 106));
+	    result.add(new TilePosition(111, 110));
+	    result.add(new TilePosition(111, 114));
+	    result.add(new TilePosition(105, 110));
+	    result.add(new TilePosition(105, 114));
+	    result.add(new TilePosition(105, 118));
+	    result.add(new TilePosition(111, 118));
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	    result.add(new TilePosition(0, 101));
+	    result.add(new TilePosition(0, 105));
+	    result.add(new TilePosition(4, 106));
+	    result.add(new TilePosition(0, 109));
+	    result.add(new TilePosition(7, 109));
+	    result.add(new TilePosition(13, 105));
+	    result.add(new TilePosition(13, 109));
+	    result.add(new TilePosition(13, 113));
+	    result.add(new TilePosition(12, 117));
+	    result.add(new TilePosition(12, 121));
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	    result.add(new TilePosition(0, 24));
+	    result.add(new TilePosition(0, 20));
+	    result.add(new TilePosition(0, 16));
+	    result.add(new TilePosition(7, 16));
+	    result.add(new TilePosition(7, 20));
+	    result.add(new TilePosition(13, 20));
+	    result.add(new TilePosition(14, 16));
+	    result.add(new TilePosition(8, 12));
+	    result.add(new TilePosition(14, 12));
+	    result.add(new TilePosition(14, 8));
+	}
+
+	return result;
+    }
+
+    // 본진 입구 벙커를 지을 위치를 설정한다.
+    @Override
+    public List<TilePosition> initBaseEntranceBunker() {
+	List<TilePosition> result = new ArrayList<>();
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	    result.add(new TilePosition(122, 25));
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	    result.add(new TilePosition(122, 102));
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	    result.add(new TilePosition(4, 102));
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	    result.add(new TilePosition(4, 24));
+	}
+
+	return result;
+    }
+
+    // 3*2 사이즈 건물을 짓기 위한 위치들을 설정한다. (서플라이 디팟, 아마데미 등)
+    public List<TilePosition> init3by2SizeBuildings() {
+	List<TilePosition> result = new ArrayList<>();
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	    result.add(new TilePosition(125, 25));
+	    result.add(new TilePosition(125, 23));
+	    result.add(new TilePosition(125, 21));
+	    result.add(new TilePosition(125, 19));
+	    result.add(new TilePosition(125, 17));
+	    result.add(new TilePosition(125, 0));
+	    result.add(new TilePosition(125, 2));
+	    result.add(new TilePosition(125, 4));
+	    result.add(new TilePosition(122, 0));
+	    result.add(new TilePosition(122, 2));
+	    result.add(new TilePosition(122, 4));
+	    result.add(new TilePosition(119, 0));
+	    result.add(new TilePosition(119, 2));
+	    result.add(new TilePosition(116, 0));
+	    result.add(new TilePosition(116, 2));
+	    result.add(new TilePosition(113, 0));
+	    result.add(new TilePosition(113, 2));
+	    result.add(new TilePosition(110, 0));
+	    result.add(new TilePosition(110, 2));
+	    result.add(new TilePosition(107, 0));
+	    result.add(new TilePosition(107, 2));
+	    result.add(new TilePosition(107, 4));
+	    result.add(new TilePosition(104, 0));
+	    result.add(new TilePosition(104, 2));
+	    result.add(new TilePosition(104, 4));
+	    result.add(new TilePosition(101, 0));
+	    result.add(new TilePosition(101, 2));
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	    result.add(new TilePosition(125, 102));
+	    result.add(new TilePosition(125, 104));
+	    result.add(new TilePosition(125, 106));
+	    result.add(new TilePosition(125, 108));
+	    result.add(new TilePosition(125, 110));
+	    result.add(new TilePosition(125, 112));
+	    result.add(new TilePosition(96, 125));
+	    result.add(new TilePosition(99, 125));
+	    result.add(new TilePosition(99, 123));
+	    result.add(new TilePosition(102, 125));
+	    result.add(new TilePosition(102, 123));
+	    result.add(new TilePosition(105, 125));
+	    result.add(new TilePosition(105, 123));
+	    result.add(new TilePosition(108, 125));
+	    result.add(new TilePosition(108, 123));
+	    result.add(new TilePosition(111, 125));
+	    result.add(new TilePosition(111, 123));
+	    result.add(new TilePosition(114, 125));
+	    result.add(new TilePosition(114, 123));
+	    result.add(new TilePosition(117, 125));
+	    result.add(new TilePosition(117, 123));
+	    result.add(new TilePosition(120, 125));
+	    result.add(new TilePosition(120, 123));
+	    result.add(new TilePosition(117, 121));
+	    result.add(new TilePosition(114, 125));
+	    result.add(new TilePosition(111, 17));
+	    result.add(new TilePosition(114, 17));
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	    result.add(new TilePosition(7, 102));
+	    result.add(new TilePosition(8, 125));
+	    result.add(new TilePosition(11, 125));
+	    result.add(new TilePosition(14, 125));
+	    result.add(new TilePosition(17, 125));
+	    result.add(new TilePosition(20, 125));
+	    result.add(new TilePosition(23, 125));
+	    result.add(new TilePosition(26, 125));
+	    result.add(new TilePosition(26, 123));
+	    result.add(new TilePosition(23, 123));
+	    result.add(new TilePosition(20, 123));
+	    result.add(new TilePosition(24, 121));
+	    result.add(new TilePosition(21, 121));
+	    result.add(new TilePosition(21, 119));
+	    result.add(new TilePosition(1, 115));
+	    result.add(new TilePosition(4, 115));
+	    result.add(new TilePosition(4, 113));
+	    result.add(new TilePosition(1, 113));
+	    result.add(new TilePosition(20, 117));
+	    result.add(new TilePosition(20, 115));
+	    result.add(new TilePosition(20, 113));
+	    result.add(new TilePosition(20, 111));
+	    result.add(new TilePosition(13, 103));
+	    result.add(new TilePosition(8, 121));
+	    result.add(new TilePosition(8, 123));
+	    // 이거 두 개 위치 조금 애매함. 막힐 수 있음.
+	    result.add(new TilePosition(29, 125));
+	    result.add(new TilePosition(29, 123));
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	    result.add(new TilePosition(7, 24));
+	    result.add(new TilePosition(0, 0));
+	    result.add(new TilePosition(0, 2));
+	    result.add(new TilePosition(0, 4));
+	    result.add(new TilePosition(3, 0));
+	    result.add(new TilePosition(3, 2));
+	    result.add(new TilePosition(3, 4));
+	    result.add(new TilePosition(6, 0));
+	    result.add(new TilePosition(6, 2));
+	    result.add(new TilePosition(9, 0));
+	    result.add(new TilePosition(9, 2));
+	    result.add(new TilePosition(12, 0));
+	    result.add(new TilePosition(12, 2));
+	    result.add(new TilePosition(11, 4));
+	    result.add(new TilePosition(15, 0));
+	    result.add(new TilePosition(15, 2));
+	    result.add(new TilePosition(14, 4));
+	    result.add(new TilePosition(18, 0));
+	    result.add(new TilePosition(18, 2));
+	    result.add(new TilePosition(17, 4));
+	    result.add(new TilePosition(21, 0));
+	    result.add(new TilePosition(21, 2));
+	    result.add(new TilePosition(20, 4));
+	    result.add(new TilePosition(24, 0));
+	    result.add(new TilePosition(24, 2));
+	    result.add(new TilePosition(23, 4));
+	    result.add(new TilePosition(20, 6));
+	}
+
+	return result;
+    }
+
+    // 본진 가스를 짓기 위한 위치를 설정한다.
+    @Override
+    public List<TilePosition> initBaseRefinery() {
+	List<TilePosition> result = new ArrayList<>();
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	    result.add(new TilePosition(116, 4));
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	    result.add(new TilePosition(117, 113));
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	    result.add(new TilePosition(7, 113));
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	    result.add(new TilePosition(7, 4));
+	}
+
+	return result;
+    }
+
+    // 본진 입구에 위치한 터렛의 위치를 설정한다.
+    @Override
+    public List<TilePosition> initBaseTurret() {
+	// TODO 아직 미 구현됨.
+	List<TilePosition> result = new ArrayList<>();
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	}
+
+	return result;
+    }
+
+    // 본진 입구 방어를 위한 위치를 설정한다.
+    @Override
+    public TilePosition initBaseEntranceChokePoint() {
+	TilePosition result = null;
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	    result = new TilePosition(122, 26);
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	    result = new TilePosition(123, 101);
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	    result = new TilePosition(6, 101);
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	    result = new TilePosition(4, 25);
+	}
+
+	return result;
+    }
+
+    // 앞마당 입구 방어를 위한 위치를 설정한다.
+    @Override
+    public TilePosition initFirstExtensionChokePoint() {
+	TilePosition result = null;
+
+	if (allianceBaseLocation.equals(getBaseLocations(ONE_CLOCK))) {
+	    // 1시
+	    result = new TilePosition(109, 34);
+	} else if (allianceBaseLocation.equals(getBaseLocations(FIVE_CLOCK))) {
+	    // 5시
+	    result = new TilePosition(109, 93);
+	} else if (allianceBaseLocation.equals(getBaseLocations(SEVEN_CLOCK))) {
+	    // 7시
+	    result = new TilePosition(18, 93);
+	} else if (allianceBaseLocation.equals(getBaseLocations(ELEVEN_CLOCK))) {
+	    // 11시
+	    result = new TilePosition(18, 34);
+	}
+
+	return result;
+    }
+
+    @Override
+    public List<TilePosition> initEngineeringBay() {
+	return new ArrayList<TilePosition>();
+    }
+
+    @Override
+    public List<TilePosition> initFirstExpansionTurret() {
+	return new ArrayList<TilePosition>();
+    }
+
+    @Override
+    public Set<TilePosition> getHillTilePosition() {
+	Set<TilePosition> result = new HashSet<>();
+
+	return result;
+    }
+
+//    @Override
+//    public TilePosition initFirstExpansionLocaion() {
+//
+//	double tempDistance;
+//	double closestDistance = 1000000000;
+//	for (BaseLocation targetBaseLocation : BWTA.getBaseLocations()) {
+//	    if (targetBaseLocation.getTilePosition().equals(allianceBaseLocation))
+//		continue;
+//	    tempDistance = BWTA.getGroundDistance(allianceBaseLocation, targetBaseLocation.getTilePosition());
+//	    if (tempDistance < closestDistance && tempDistance > 0) {
+//		closestDistance = tempDistance;
+//		allianceFirstExpansionLocation = targetBaseLocation.getTilePosition();
+//	    }
+//	}
+//	return allianceFirstExpansionLocation;
+//    }
+}
---
+++ JAVA/BasicBot/src/StrategyDefense.java
@@ -0,0 +1,195 @@
+import java.util.Set;
+
+import bwapi.TilePosition;
+import bwapi.UnitType;
+
+public class StrategyDefense extends StrategyBase {
+
+    private StrategyManager strategyManager = null;
+    private LocationManager locationManager = null;
+    private EliminateManager eliminateManager = null;
+
+    private int totalAttackFrame = 0;
+
+    @Override
+    public void onStart(GameStatus gameStatus) {
+	super.onStart(gameStatus);
+
+	strategyManager = gameStatus.getStrategyManager();
+	locationManager = gameStatus.getLocationManager();
+	eliminateManager = gameStatus.getEliminateManager();
+    }
+
+    @Override
+    public void onFrame() {
+	super.onFrame();
+
+	// 5초에 한 번씩 수행한다.
+	if (gameStatus.isMatchedInterval(5)) {
+	    doWholeAttack();
+	}
+    }
+
+    // 전체 공격을 처리한다.
+    void doWholeAttack() {
+	// 모든 공격 가능한 유닛셋을 가져온다.
+	System.out.println("전쟁");
+	Set<Unit2> attackableUnitSet = allianceUnitInfo.getUnitSet(UnitKind.Combat_Unit);
+	// 총 공격 전이고, 공격 유닛이 60마리 이상이고, 적 본진을 발견했으면 총 공격 모드로 변환한다.
+	Log.debug("총 공격 조건 확인. 공격 위치: %s, 아군 공격 가능한 유닛 수: %d, 적 본진 위치: %s", strategyManager.getAttackTilePositon(), attackableUnitSet.size(),
+		locationManager.getEnemyBaseLocation());
+
+	if (gameStatus.getFrameCount() > 42 * 60 * 15 && 0 == enemyUnitInfo.getUnitSet(UnitKind.Building).size()) {
+	    // 15분이 넘는 시점부터는 적 건물이 없으면 eliminate 모드로 동작한다.
+	    strategyManager.clearAttackTilePosition();
+	    eliminateManager.search(allianceUnitInfo);
+	    Log.info("Eliminate Manager 동작 시작.");
+	} else if (true == strategyManager.hasAttackTilePosition() || (attackableUnitSet.size() > 10 && null != locationManager.getEnemyBaseLocation())) {
+	    Log.info("총 공격 모드로 전환. 아군 유닛 수: %d", attackableUnitSet.size());
+	    TilePosition attackTilePosition = calcAttackPosition();
+
+	    if (null != attackTilePosition) {
+		strategyManager.setAttackTilePosition(attackTilePosition);
+		Log.info("총 공격! 공격할 위치: %s", attackTilePosition);
+		for (Unit2 attackableUnit : attackableUnitSet) {
+		    ActionUtil.attackPosition(allianceUnitInfo, attackableUnit, attackTilePosition.toPosition());
+		}
+	    }
+	}
+    }
+
+    // 공격갈 위치를 계산한다.
+    private TilePosition calcAttackPosition() {
+	TilePosition result = null;
+
+	if (0 == totalAttackFrame) {
+	    result = locationManager.getFirstExtensionChokePoint();
+	    // 10초 동안 앞마당으로 집결한 뒤 총 공격을 간다.
+	    totalAttackFrame = gameStatus.getFrameCount() + 10 * 42;
+	    Log.info("앞으로 10초 뒤에 총 공격을 시작한다. 일단 앞마당에 집결하자.");
+	} else {
+	    if (totalAttackFrame < gameStatus.getFrameCount()) {
+		// 내 본진의 위치
+		TilePosition allianceStartTilePosition = locationManager.getAllianceBaseLocation();
+
+		// 적 본진의 위치
+		Set<Unit2> enemyMainBuildingSet = enemyUnitInfo.getUnitSet(UnitKind.MAIN_BUILDING);
+
+		// 가급적 본진에서 가장 가까운 적 본진부터 공격한다.
+		Unit2 closestMainBuilding = enemyUnitInfo.getClosestUnitWithLastTilePosition(enemyMainBuildingSet, allianceStartTilePosition.toPosition());
+
+		if (null != closestMainBuilding) {
+		    // 적 메인 건물이 존재할 경우..
+		    result = enemyUnitInfo.getLastTilePosition(closestMainBuilding);
+		} else {
+		    // 적 메인 건물은 찾지 못했지만, 다른 건물들이 존재할 경우.
+		    Set<Unit2> enemyBuildingSet = enemyUnitInfo.getUnitSet(UnitKind.Building);
+		    if (!enemyBuildingSet.isEmpty()) {
+			// 적 건물이 다수 존재할 경우, 내 본진에서 가장 가까운 상대 건물부터 공격한다.
+			Unit2 closestBuilding = enemyUnitInfo.getClosestUnitWithLastTilePosition(enemyBuildingSet, allianceStartTilePosition.toPosition());
+			if (null != closestBuilding) {
+			    result = enemyUnitInfo.getLastTilePosition(closestBuilding);
+			}
+		    } else {
+			TilePosition enemyBaseLocation = locationManager.getEnemyBaseLocation();
+			if (gameStatus.isExplored(enemyBaseLocation)) {
+			    Log.info("적 건물을 찾을 수 없다. 탐색하자");
+			    result = null;
+			} else {
+			    // 어떠한 적 건물도 찾지 못했지만, 적 본진 위치는 알고 있을 경우 (예를 들어 3곳을 정찰 성공했다면 남은 한 곳은 방문하지 않아도 적 본진이다.)
+			    Log.info("적 건물은 없지만, 적 위치는 알고 있다. 적 위치로 공격을 가자: %s", enemyBaseLocation);
+			    result = enemyBaseLocation;
+			}
+		    }
+		}
+		Log.info("총 공격한다: %s", result);
+	    } else {
+		result = locationManager.getFirstExtensionChokePoint();
+		Log.info("내 본진 앞마당으로 집결한다");
+	    }
+	}
+
+	return result;
+    }
+
+    @Override
+    public void initialBuildOrder() {
+	BuildManager buildManager = gameStatus.getBuildManager();
+
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	
+	//9서플
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Supply_Depot));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	
+	//11배럭, 가스
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Barracks));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Refinery));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	
+	//12서치
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.SCOUTING));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.GATHER_GAS));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.GATHER_GAS));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	
+	//15팩
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Factory));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Supply_Depot));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	
+	//20커맨드 18가스
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Command_Center));
+	//애드온
+	
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_Marine));
+	
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Supply_Depot));
+	
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_Marine));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_Marine));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_Marine));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.TRAINING, UnitType.Terran_SCV));
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.BUILD, UnitType.Terran_Bunker));
+	
+	buildManager.add(new BuildOrderItem(BuildOrderItem.Order.INITIAL_BUILDORDER_FINISH));
+    }
+
+    @Override
+    public void initialStrategyItem(Set<StrategyItem> strategyItems) {
+	
+	strategyItems.add(StrategyItem.AUTO_LOAD_MARINE_TO_BUNKER);
+	strategyItems.add(StrategyItem.AUTO_REPAIR_BUNKER);
+	strategyItems.add(StrategyItem.AUTO_TRAIN_MECHANIC_UNIT);
+	strategyItems.add(StrategyItem.SET_FACTORY_RALLY);
+	
+//	strategyItems.add(StrategyItem.AUTO_TRAIN_BIONIC_UNIT);
+//	strategyItems.add(StrategyItem.SET_BARRACKS_RALLY);
+//	strategyItems.add(StrategyItem.AUTO_RESEARCH_U_238_Shells);
+//	strategyItems.add(StrategyItem.AUTO_RESEARCH_STIMPACK);
+//	strategyItems.add(StrategyItem.AUTO_UPGRADE_BIONIC_UNIT);
+	
+	strategyItems.add(StrategyItem.AUTO_ADDON_COMSAT_STATION);
+	strategyItems.add(StrategyItem.AUTO_ADDON_MACHINE_SHOP);
+	strategyItems.add(StrategyItem.AUTO_USING_SCAN);
+	strategyItems.add(StrategyItem.AUTO_DEFENCE_ALLIANCE_BASE);
+	strategyItems.add(StrategyItem.AUTO_LIFT_COMMAND_CENTER);
+	//strategyItems.add(StrategyItem.AGGRESSIVE_MOVE_ATTACK);
+    }
+
+}
